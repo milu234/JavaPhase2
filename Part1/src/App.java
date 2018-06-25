0e940af17b829b20a654270ac77ff5efcdd15958
milu234 <milanhazra234.mh@gmail.com>
2018-06-25 23:00:58 +0530

    Long

--- Part1/src/App.java
+++ Part1/src/App.java
@@ -6,8 +6,10 @@ public class App {
 		int myNumber ;
 		myNumber = 78;
 		short myShort = 874;
+		long myLong = 98745;
 		System.out.println(myNumber);
 		System.out.println(myShort);
+		System.out.println(myLong);
 
 	}
 
