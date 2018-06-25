13ca28890ea73b5f69a22c67248e7e6e0c7399ee
milu234 <milanhazra234.mh@gmail.com>
2018-06-25 22:48:37 +0530

    Variables

--- Part1/src/App.java
+++ Part1/src/App.java
@@ -3,7 +3,9 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		int myNumber ;
+		myNumber = 78;
+		System.out.println(myNumber);
 
 	}
 
