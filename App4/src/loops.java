9ab5356d241e64f8e046a5f22b0046ec53f28775
milu234 <milanhazra234.mh@gmail.com>
2018-06-26 12:34:55 +0530

    loops

--- App4/src/loops.java
+++ App4/src/loops.java
@@ -1,9 +1,12 @@
 
 public class loops {
 	public static void main(String[] args) {
-		boolean loop = true;
+		int value = 0;
 		
-		System.out.println(loop);
+		while(value < 10) {
+			System.out.println("Hello" +" "+ value);
+			value = value+1;
+		}
 	}
 
 }
