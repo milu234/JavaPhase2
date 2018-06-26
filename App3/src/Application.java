467c143f4cf9df5f6ecb30eea5bf85e397169712
milu234 <milanhazra234.mh@gmail.com>
2018-06-26 11:50:50 +0530

    non Primitive

---
+++ App3/src/Application.java
@@ -0,0 +1,13 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 7;
+		
+		String text = "Hello";
+		String blank = " ";
+		String name ="BOB";
+		
+		String greeting  = text + blank+name;
+		System.out.println(greeting);
+	}
+}
