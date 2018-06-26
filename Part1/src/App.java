f5f94a806f019c2c41ee3cd457bbbf4877055de8
milu234 <milanhazra234.mh@gmail.com>
2018-06-26 11:30:14 +0530

    my Boolean

--- Part1/src/App.java
+++ Part1/src/App.java
@@ -10,6 +10,7 @@ public class App {
 		double myDouble = 7.74589;
 		float myFloat = 7.32f;
 		char myChar = 'y';
+		boolean myBoolean = true;
 		
 		System.out.println(myNumber);
 		System.out.println(myShort);
@@ -17,6 +18,7 @@ public class App {
 		System.out.println(myDouble);
 		System.out.println(myFloat);
 		System.out.println(myChar);
+		System.out.println(myBoolean);
 
 	}
 
