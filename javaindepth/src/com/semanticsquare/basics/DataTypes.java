b657dcbafbc4fdb269c6ed2723ec19610e8c2a58
milu234 <milanhazra234.mh@gmail.com>
2018-12-25 17:19:53 +0530

    Type casting Done

---
+++ javaindepth/src/com/semanticsquare/basics/DataTypes.java
@@ -0,0 +1,13 @@
+class DataTypes{
+	public static void main(String[] args){
+		long  m = 40;
+		int n = (int)m;
+		System.out.println(m);
+		System.out.println(n);
+		byte b = 65;
+		char c = (char)b; // c='A'
+		System.out.println(c);
+		char d = 65;
+		System.out.println(d);
+	}
+}
