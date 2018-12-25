28b1581225591ba18d013c5a855ca470876479fe
milu234 <milanhazra234.mh@gmail.com>
2018-12-25 17:27:16 +0530

    Type casting Done

--- javaindepth/src/com/semanticsquare/basics/DataTypes.java
+++ javaindepth/src/com/semanticsquare/basics/DataTypes.java
@@ -7,7 +7,16 @@ class DataTypes{
 		byte b = 65;
 		char c = (char)b; // c='A'
 		System.out.println(c);
-		char d = 65;
+		char d = 65; //c = 'A'
 		System.out.println(d);
-	}
+		short e = 'A';
+		System.out.println(e);
+		int x = (int)3.14f ; 
+		System.out.println(x);
+		int y  = (int)0.9;
+		System.out.println(y);
+		
+		char z = (char)65.5; // c = 'A'
+		System.out.println(z);
+	}	
 }
