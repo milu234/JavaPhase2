1715058ab0e72a0942baa7589cc9e443b2a4a7f5
milu234 <milanhazra234.mh@gmail.com>
2019-01-12 03:43:00 +0530

    Objects reference

---
+++ javaindepth/src/com/semanticsquare/basics/Over.java
@@ -0,0 +1,16 @@
+class Over {
+	
+	static void go(int[] array){
+		System.out.println("array[0]: " + array[0]);
+		System.out.println("array[0]: " + array[1]);
+		System.out.println("array[0]: " + array[2]);
+		
+		
+	}
+	
+	public static void main(String[] args){
+		int[] array = {1,2,3};
+		go(array);
+	}
+	
+}
