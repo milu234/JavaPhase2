61c4d152d9097975d4b072abf8f6b3347640f612
milu234 <milanhazra234.mh@gmail.com>
2019-01-26 03:44:28 +0530

    Overloading Done

--- javaindepth/src/com/semanticsquare/basics/Over.java
+++ javaindepth/src/com/semanticsquare/basics/Over.java
@@ -10,11 +10,23 @@ class Over {
 		
 	}
 	
+	static void go(int i){
+		System.out.println("Int I");
+	}
+	
+	static void go(short s){
+		System.out.println("Short s");
+		
+	}
+	
+	
 	public static void main(String[] args){
 		int[] array = {1,2,3};
 		go(array);
 		System.out.println("array[2]: " + array[1]);
 		
+		Byte h = 25;
+		go(h);
 		
 	}
 	
